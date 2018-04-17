169594b85165466d9bb6e58e992c3bea8ed718d6
Leo <Leo@Manian>
2018-04-17 16:06:52 +0200

    ajout d'une package + class

---
+++ tzetr/src/bordel/Petard.java
@@ -0,0 +1,14 @@
+/*
+ * Petard.java                                                          17 avr. 2018
+ * IUT info1 2017-2018 groupe 3, ni copyright ni copyleft 
+ */
+package bordel;
+
+/**
+ * TODO commenter les responsabilités de cette classe
+ * @author Leo
+ *
+ */
+public class Petard {
+
+}
